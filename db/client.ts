daddd5f5dd5f5ddb1f5d44a9a16755e0e5e1edb6
Andrés Villarreal <andres@villarreal.co.cr>
2024-10-27 08:00:32 +0100

    preparing a deploy

--- db/client.ts
+++ db/client.ts
@@ -1,13 +1,24 @@
 import { Pool } from "postgres";
+import "dotenv";
 
 const POOL_SIZE = 10;
 
+// Import dotenv to load environment variables
+import "https://deno.land/x/dotenv@v3.2.0/load.ts";
+
+// Access environment variables
+const dbHost = Deno.env.get("DB_HOST") || "localhost";
+const dbUser = Deno.env.get("DB_USER") || "musicbrainz";
+const dbPassword = Deno.env.get("DB_PASSWORD") || "musicbrainz";
+const dbName = Deno.env.get("DB_NAME") || "musicbrainz";
+const dbPort = Deno.env.get("DB_PORT") || "15432";
+
 const pool = new Pool({
-  user: "musicbrainz",
-  password: "musicbrainz",
-  database: "musicbrainz",
-  hostname: "localhost",
-  port: 15432,
+  user: dbUser,
+  password: dbPassword,
+  database: dbName,
+  hostname: dbHost,
+  port: Number(dbPort),
 }, POOL_SIZE);
 
 export default pool;
---
+++ infra/connect.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ssh -i "music-graph2.pem" ec2-user@ec2-18-199-122-244.eu-central-1.compute.amazonaws.com
