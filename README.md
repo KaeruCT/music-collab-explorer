abe9bf079268fb4d07dd42c922761810d73296c3
Andrés Villarreal <andres@villarreal.co.cr>
2025-02-23 22:06:37 +0100

    add script to populate db

--- README.md
+++ README.md
@@ -12,26 +12,14 @@ deno task build
 ```sh
 deno task start
 ```
-## steps to fill postgres database
+## populate database
 
-1. Download https://musicbrainz.org/doc/MusicBrainz_Database/Download#mbdump.tar.bz2 and place in `setup/`
-2. Create database locally
-```sql
-CREATE USER musicbrainz WITH PASSWORD 'musicbrainz';
-CREATE DATABASE musicbrainz;
-ALTER DATABASE musicbrainz OWNER TO musicbrainz;
-ALTER USER musicbrainz WITH SUPERUSER; -- or GRANT rds_superuser TO musicbrainz; in AWS RDS
-GRANT ALL PRIVILEGES ON DATABASE musicbrainz TO musicbrainz;
-GRANT USAGE ON SCHEMA public TO musicbrainz;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO musicbrainz;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO musicbrainz;
-```
-3. Init:
+1. Download MusicBrainz Database dump and import locally
 ```sh
 cd setup/
 ./init_db.sh
-```
-4. (Optional) Remove superuser from musicbrainz user
+
+1. (Optional) Remove superuser from musicbrainz user
 ```sql
 ALTER USER musicbrainz WITH NOSUPERUSER;
 ```
--- setup/init_db.sh
+++ setup/init_db.sh
@@ -1,71 +1,99 @@
 #!/bin/bash
-# heavily based on https://github.com/arey/musicbrainz-database/blob/master/create-database.sh
+set -xe
 
-export PGHOST=localhost
-export PGPORT=15432
-export PGPASSWORD=postgres
-export DB=musicbrainz
-export PGUSER=postgres
+BASE_URL="https://data.metabrainz.org/pub/musicbrainz/data/fullexport"
+LATEST_FILE="$BASE_URL/LATEST"
 
-# Define an array of tables to import
+# Get the latest dump version
+LATEST_VERSION=$(curl -s "$LATEST_FILE")
+if [[ -z "$LATEST_VERSION" ]]; then
+    echo "Failed to retrieve the latest version."
+    exit 1
+fi
+
+DUMP_URL="$BASE_URL/$LATEST_VERSION/mbdump.tar.bz2"
+FILE_NAME="mbdump.tar.bz2"
+
+# Download using wget with resume support
+wget -c "$DUMP_URL" -O "$FILE_NAME"
+
+echo "MusicBrainz Database dump downloaded"
+
+PGHOST=localhost
+PGUSER=postgres
+PGPORT=15432
+DBNAME=musicbrainz
+DBUSER=musicbrainz
+DBPASS=musicbrainz
+
+echo "(Re)creating database and user"
+
+psql --no-psqlrc -p $PGPORT -h $PGHOST -U "$PGUSER" -c "DROP DATABASE IF EXISTS $DBNAME;"
+psql --no-psqlrc -p $PGPORT -h $PGHOST -U "$PGUSER" -c "DO \$\$ BEGIN IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = '$DBUSER') THEN CREATE USER $DBUSER WITH PASSWORD '$DBPASS'; END IF; END \$\$;"
+psql --no-psqlrc -p $PGPORT -h $PGHOST -U "$PGUSER" -c "CREATE DATABASE $DBNAME OWNER $DBUSER;"
+psql --no-psqlrc -p $PGPORT -h $PGHOST -U "$PGUSER" -c "ALTER USER $DBUSER WITH SUPERUSER;"
+psql --no-psqlrc -p $PGPORT -h $PGHOST -U "$PGUSER" -c "GRANT ALL PRIVILEGES ON DATABASE $DBNAME TO $DBUSER;"
+psql --no-psqlrc -p $PGPORT -h $PGHOST -U "$PGUSER" -d "$DBNAME" -c "GRANT USAGE ON SCHEMA public TO $DBUSER;"
+psql --no-psqlrc -p $PGPORT -h $PGHOST -U "$PGUSER" -d "$DBNAME" -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO $DBUSER;"
+psql --no-psqlrc -p $PGPORT -h $PGHOST -U "$PGUSER" -d "$DBNAME" -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO $DBUSER;"
+
+# the next section is heavily based on https://github.com/arey/musicbrainz-database/blob/master/create-database.sh
+
+psql --no-psqlrc -p $PGPORT -h $PGHOST -d $DBNAME -U $PGUSER -a -c "CREATE SCHEMA IF NOT EXISTS musicbrainz;";
+
+# not sure why this doesn't work so we create a different way
+# wget https://raw.githubusercontent.com/metabrainz/musicbrainz-server/master/admin/sql/CreateCollations.sql
+# psql --no-psqlrc -p $PGPORT -h $PGHOST -d $DBNAME -U $PGUSER -a -f CreateCollations.sql
+# rm CreateCollations.sql
+##psql --no-psqlrc -p $PGPORT -h $PGHOST -d $DBNAME -U $PGUSER -a -c "CREATE COLLATION public.musicbrainz (LOCALE = 'C');"
+psql --no-psqlrc -p $PGPORT -h $PGHOST -d $DBNAME -U $PGUSER -a -c "CREATE COLLATION musicbrainz.musicbrainz (LOCALE = 'C');"
+
+# fetch and modify SQL files before execution
+fetch_and_execute_sql() {
+    local sql_url=$1
+    local sql_file=$(basename "$sql_url")
+    wget "$sql_url"
+    awk 'NR==1{print "SET search_path TO musicbrainz, public;"}1' "$sql_file" > tmp.sql && mv tmp.sql "$sql_file"
+    psql --no-psqlrc -p $PGPORT -h $PGHOST -d $DBNAME -U $PGUSER -a -f "$sql_file"
+    rm "$sql_file"
+}
+
+fetch_and_execute_sql "https://raw.githubusercontent.com/metabrainz/musicbrainz-server/master/admin/sql/Extensions.sql"
+fetch_and_execute_sql "https://raw.githubusercontent.com/metabrainz/musicbrainz-server/master/admin/sql/CreateTypes.sql"
+fetch_and_execute_sql "https://raw.githubusercontent.com/metabrainz/musicbrainz-server/master/admin/sql/CreateTables.sql"
+fetch_and_execute_sql "https://raw.githubusercontent.com/metabrainz/musicbrainz-server/master/admin/sql/CreateSearchConfiguration.sql"
+fetch_and_execute_sql "https://raw.githubusercontent.com/metabrainz/musicbrainz-server/master/admin/sql/CreateFunctions.sql"
+
+# tables to import
 tables=("artist" "artist_credit_name" "artist_credit" "track")
 
-# Construct a list of files to extract
 files_to_extract=()
 for table in "${tables[@]}"; do
   files_to_extract+=("mbdump/${table}")
 done
 
-psql -p $PGPORT -h $PGHOST -d $DB -U $PGUSER -a -c "CREATE SCHEMA musicbrainz;";
-# not sure why this doesn't work so we create a different way
-# wget https://raw.githubusercontent.com/metabrainz/musicbrainz-server/master/admin/sql/CreateCollations.sql
-# psql -p $PGPORT -h $PGHOST -d $DB -U $PGUSER -a -f CreateCollations.sql
-# rm CreateCollations.sql
-psql -p $PGPORT -h $PGHOST -d $DB -U $PGUSER -a -c "CREATE COLLATION musicbrainz (LOCALE = 'C');"
-
-wget https://raw.githubusercontent.com/metabrainz/musicbrainz-server/master/admin/sql/Extensions.sql
-psql -p $PGPORT -h $PGHOST -d $DB -U $PGUSER -a -f Extensions.sql
-rm Extensions.sql
-
-wget https://raw.githubusercontent.com/metabrainz/musicbrainz-server/master/admin/sql/CreateFunctions.sql
-psql -p $PGPORT -h $PGHOST -d $DB -U $PGUSER -a -f CreateFunctions.sql
-rm CreateFunctions.sql
-
-wget https://raw.githubusercontent.com/metabrainz/musicbrainz-server/master/admin/sql/CreateTypes.sql
-psql -p $PGPORT -h $PGHOST -d $DB -U $PGUSER -a -f CreateTypes.sql
-rm CreateTypes.sql
-
-wget https://raw.githubusercontent.com/metabrainz/musicbrainz-server/master/admin/sql/CreateTables.sql
-psql -p $PGPORT -h $PGHOST -d $DB -U $PGUSER -a -f CreateTables.sql
-rm CreateTables.sql
-
-echo "decompressing Musicbrainz dump"
-# Extract only the selected files
-tar xjvf mbdump.tar.bz2 "${files_to_extract[@]}"
+echo "Extracting tables from dump"
+# extract only the selected files
+tar xjvf "$FILE_NAME" "${files_to_extract[@]}"
 
 for f in "${files_to_extract[@]}"
 do
   tablename="${f:7}"
-  echo "importing $tablename table"
+  echo "Importing table: $tablename"
   chmod a+rX "$f"
-  (echo "SET search_path TO musicbrainz, public;" && echo "\COPY $tablename FROM '$f'") | psql -p $PGPORT -h $PGHOST -d $DB -U $PGUSER -a
+  (echo "SET search_path TO musicbrainz, public;" && echo "\COPY $tablename FROM '$f'") | psql --no-psqlrc -p $PGPORT -h $PGHOST -d $DBNAME -U $PGUSER -a
 done
 
 rm -rf mbdump
 
-echo "creating indexes and primary Keys"
+echo "Creating indexes and primary keys"
 
-wget https://raw.githubusercontent.com/metabrainz/musicbrainz-server/master/admin/sql/CreatePrimaryKeys.sql
-psql -p $PGPORT -h $PGHOST -d $DB -U $PGUSER -a -f CreatePrimaryKeys.sql
-rm CreatePrimaryKeys.sql
+fetch_and_execute_sql "https://raw.githubusercontent.com/metabrainz/musicbrainz-server/master/admin/sql/CreatePrimaryKeys.sql"
+fetch_and_execute_sql "https://raw.githubusercontent.com/metabrainz/musicbrainz-server/master/admin/sql/CreateIndexes.sql"
 
-wget https://raw.githubusercontent.com/metabrainz/musicbrainz-server/master/admin/sql/CreateIndexes.sql
-psql -p $PGPORT -h $PGHOST -d $DB -U $PGUSER -a -f CreateIndexes.sql
-rm CreateIndexes.sql
+psql --no-psqlrc -p $PGPORT -h $PGHOST -d $DBNAME -U $PGUSER -a -c "CREATE INDEX IF NOT EXISTS idx_artist_credit_name_artist ON musicbrainz.artist_credit_name (artist);"
 
-psql -p $PGPORT -h $PGHOST -d $DB -U $PGUSER -a -c "CREATE INDEX idx_artist_credit_name_artist ON artist_credit_name (artist);"
+psql --no-psqlrc -p $PGPORT -h $PGHOST -d $DBNAME -U $PGUSER -a -c "CREATE INDEX IF NOT EXISTS idx_artist_credit_name_artist_credit ON musicbrainz.artist_credit_name (artist_credit);"
 
-psql -p $PGPORT -h $PGHOST -d $DB -U $PGUSER -a -c "CREATE INDEX idx_artist_credit_name_artist_credit ON artist_credit_name (artist_credit);"
-
-psql -p $PGPORT -h $PGHOST -d $DB -U $PGUSER -a -c "CREATE EXTENSION IF NOT EXISTS pg_trgm; CREATE INDEX idx_artist_name_trgm ON artist USING gin (name gin_trgm_ops); CREATE INDEX idx_acn_name_trgm ON artist_credit_name USING gin (name gin_trgm_ops);";
+psql --no-psqlrc -p $PGPORT -h $PGHOST -d $DBNAME -U $PGUSER -a -c "CREATE EXTENSION IF NOT EXISTS pg_trgm; CREATE INDEX IF NOT EXISTS idx_artist_name_trgm ON musicbrainz.artist USING gin (name gin_trgm_ops); CREATE INDEX IF NOT EXISTS idx_acn_name_trgm ON musicbrainz.artist_credit_name USING gin (name gin_trgm_ops);";
 
